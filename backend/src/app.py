bb9fb234f4745cbd0f71f3d7b5f85bd3f1f17a3c
hammerhc <kriegersimon4@gmail.com>
2021-06-02 09:15:24 +0200

    Changed app.py
    API changes
    Use of controllers

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,350 +6,298 @@ app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:secret@localhost:5432/cars"
 db = SQLAlchemy(app)
 
-from models import *
+import controllers.cars as cars
+import controllers.account as account
+import controllers.messages as messages
+import controllers.orders as orders
+import controllers.roles as roles
+import controllers.states as states
+import controllers.tickets as tickets
+import controllers.users as users
+
+@app.errorhandler(400)
+def bad_request(error):
+    result = {
+        "message": "{error}",
+        "data": [],
+        "count": 0,
+        "error": True
+    }
+
+    return {"result": result}
+
+
+@app.errorhandler(401)
+def unauthorized(error):
+    result = {
+        "message": "{error}",
+        "data": [],
+        "count": 0,
+        "error": True
+    }
+
+    return {"result": result}
+
+@app.errorhandler(403)
+def forbidden(error):
+    result = {
+        "message": "{error}",
+        "data": [],
+        "count": 0,
+        "error": True
+    }
+
+    return {"result": result}
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    result = {
+        "message": "{error}",
+        "data": [],
+        "count": 0,
+        "error": True
+    }
+
+    return {"result": result}
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    result = {
+        "message": "{error}",
+        "data": [],
+        "count": 0,
+        "error": True
+    }
+
+    return {"result": result}
+
+@app.errorhandler(500)
+def server_error(error):
+    result = {
+        "message": "{error}",
+        "data": [],
+        "count": 0,
+        "error": True
+    }
+
+    return {"result": result}
+
+@app.route("/login", methods=['POST'])
+def request_login():
+    result = []
+
+    if request.method == "POST":
+        result = account.login()
+
+    return {"result": result}
+
+@app.route("/register", methods=['POST'])
+def request_register():
+    result = []
+
+    if request.method == "POST":
+        result = account.register()
+
+    return {"result": result}
+
+@app.route("/logout", methods=['POST'])
+def request_logout():
+    result = []
+
+    if request.method == "POST":
+        result = account.logout()
+
+    return {"result": result}
 
 @app.route("/user", methods=['POST', 'GET'])
-def request_user():
-    error = False
+def request_users():
+    result = []
+
     if request.method == 'POST':
+        result = users.create_user()
 
-        if request.is_json:
-            data = request.get_json()
-            new_user = User(
-                username=data['username'], password=data['password'], role_id=data['role_id'])
-            db.session.add(new_user)
-            db.session.commit()
-            result = [
-                {
-                    "message": f"User {new_user.username} has been created successfully.",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
-        else:
-            error = True
-            result = [
-                {
-                    "message": "The request payload is not in JSON format",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
+    if request.method == "GET":
+        result = users.get_users()
 
-    elif request.method == 'GET':
-        users = User.query.all()
-        data = [
-            {
-                "id": user.id,
-                "username": user.username,
-                "password": user.password,
-                "role_id": user.role_id
-            } for user in users
-        ]
+    return {"result": result}
 
-        result = [
-            {
-                "message": "Success",
-                "data": data,
-                "count": len(data),
-                "error": error
-            }
-        ]
 
-        return {"result": result}
+@app.route("/user/<id>", methods=['PUT', 'GET', 'DELETE'])
+def request_user(id):
+    result = []
+
+    if request.method == "PUT":
+        result = users.edit_user(id)
+
+    if request.method == "GET":
+        result = users.get_user(id)
+
+    if request.method == "DELETE":
+        result = users.delete_user(id)
+
+    return {"result": result}
 
 
 @app.route("/role", methods=['POST', 'GET'])
-def request_role():
-    error = False
-    if request.method == 'POST':
+def request_roles():
+    result = []
 
-        if request.is_json:
-            data = request.get_json()
-            new_role = Role(name=data['name'])
-            db.session.add(new_role)
-            db.session.commit()
-            result = [
-                {
-                    "message": f"Role {new_role.name} has been created successfully.",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
-        else:
-            error = True
-            result = [
-                {
-                    "message": "The request payload is not in JSON format",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
+    if request.method == "POST":
+        result = roles.create_role()
 
-    elif request.method == 'GET':
-        roles = Role.query.all()
-        data = [
-            {
-                "id": role.id,
-                "name": role.name
-            } for role in roles
-        ]
+    if request.method == "GET":
+        result = roles.get_roles()
 
-        result = [
-            {
-                "message": "Success",
-                "data": data,
-                "count": len(data),
-                "error": error
-            }
-        ]
+    return {"result": result}
 
-        return {"result": result}
+
+@app.route("/role/<id>", methods=['PUT', 'GET', 'DELETE'])
+def request_role(id):
+    result = []
+
+    if request.method == "PUT":
+        result = roles.edit_role(id)
+
+    if request.method == "GET":
+        result = roles.get_role(id)
+
+    if request.method == "DELETE":
+        result = roles.delete_role(id)
+
+    return {"result": result}
 
 
 @app.route("/car", methods=['POST', 'GET'])
-def request_car():
-    error = False
-    if request.method == 'POST':
+def request_cars():
+    result = []
 
-        if request.is_json:
-            data = request.get_json()
-            new_car = Car(name=data['name'],
-                          model=data['model'], price=data['price'])
-            db.session.add(new_car)
-            db.session.commit()
-            result = [
-                {
-                    "message": f"Car {new_car.name} has been created successfully.",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
-        else:
-            error = True
-            result = [
-                {
-                    "message": "The request payload is not in JSON format",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
+    if request.method == "POST":
+        result = cars.create_car()
 
-    elif request.method == 'GET':
-        cars = Car.query.all()
-        data = [
-            {
-                "id": car.id,
-                "name": car.name,
-                "model": car.model,
-                "price": car.price
-            } for car in cars
-        ]
+    if request.method == "GET":
+        result = cars.get_cars()
 
-        result = [
-            {
-                "message": "Success",
-                "data": data,
-                "count": len(data),
-                "error": error
-            }
-        ]
+    return {"result": result}
 
-        return {"result": result}
+
+@app.route("/car/<id>", methods=['PUT', 'GET', 'DELETE'])
+def request_car(id):
+    result = []
+
+    if request.method == "PUT":
+        result = cars.edit_car(id)
+
+    if request.method == "GET":
+        result = cars.get_car(id)
+
+    if request.method == "DELETE":
+        result = cars.delete_car(id)
+
+    return {"result": result}
 
 
 @app.route("/order", methods=['POST', 'GET'])
-def request_order():
-    error = False
-    if request.method == 'POST':
+def request_orders():
+    result = []
 
-        if request.is_json:
-            data = request.get_json()
-            new_order = Order(customer=data['customer'], date=data['date'])
-            cars = data['cars']
+    if request.method == "POST":
+        result = orders.create_order()
 
-            db.session.add(new_order)
-            db.session.commit()
+    if request.method == "GET":
+        result = orders.get_orders()
 
-            for car in cars:
-                new_car_order = Order_Car(
-                    car=car, order=new_order.id)
-                db.session.add(new_car_order)
+    return {"result": result}
 
-            db.session.commit()
 
-            result = [
-                {
-                    "message": f"Order {new_order.id} on {new_order.date} has been created successfully.",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
-        else:
-            error = True
-            result = [
-                {
-                    "message": "The request payload is not in JSON format",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
+@app.route("/order/<id>", methods=['GET', 'DELETE'])
+def request_order(id):
+    result = []
 
-    elif request.method == 'GET':
-        orders = Order.query.all()
-        order_cars = Order_Car.query.all()
-        cars = Car.query.all()
+    if request.method == "GET":
+        result = orders.get_order(id)
 
-        data = []
+    if request.method == "DELETE":
+        result = orders.delete_order(id)
 
-        for order in orders:
-            ordered_cars = []
-            for order_car in order_cars:
-                if order.id == order_car.order:
-                    for car in cars:
-                        if car.id == order_car.car:
-                            ordered_cars.append({
-                                "id": car.id,
-                                "name": car.name,
-                                "model": car.model,
-                                "price": car.price
-                            })
-            data.append({
-                "id": order.id,
-                "date": order.date,
-                "cars": ordered_cars
-            })
-
-        result = [
-            {
-                "message": "Success",
-                "data": data,
-                "count": len(data),
-                "error": error
-            }
-        ]
-
-        return {"result": result}
+    return {"result": result}
 
 
 @app.route("/ticket", methods=['POST', 'GET'])
-def request_ticket():
-    error = False
-    if request.method == 'POST':
+def request_tickets():
+    result = []
 
-        if request.is_json:
-            data = request.get_json()
-            new_ticket = Ticket(title=data['title'],
-                owner=data['owner'], customer=data['customer'], date=data['date'])
-            db.session.add(new_ticket)
-            db.session.commit()
-            result = [
-                {
-                    "message": f"Ticket {new_ticket.id} on {new_ticket.date} has been created successfully.",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
-        else:
-            error = True
-            result = [
-                {
-                    "message": "The request payload is not in JSON format",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
+    if request.method == "POST":
+        result = tickets.create_ticket()
 
-    elif request.method == 'GET':
-        messages = Message.query.all()
-        tickets = Ticket.query.all()
+    if request.method == "GET":
+        result = tickets.get_tickets()
 
-        data = []
+    return {"result": result}
 
-        for ticket in tickets:
-            message_list = []
-            for message in messages:
-                if ticket.id == message.ticket:
-                    message_list.append({
-                        "id": message.id,
-                        "user": message.user,
-                        "ticket": message.ticket,
-                        "message": message.message,
-                        "datetime": message.datetime
-                    })
-            
-            data.append({
-                "id": ticket.id,
-                "title": ticket.title,
-                "owner": ticket.owner,
-                "customer": ticket.customer,
-                "date": ticket.date,
-                "messages": message_list
-            })
 
-        result = [
-            {
-                "message": "Success",
-                "data": data,
-                "count": len(data),
-                "error": error
-            }
-        ]
+@app.route("/ticket/<id>", methods=['PUT', 'GET', 'DELETE'])
+def request_ticket(id):
+    result = []
 
-        return {"result": result}
+    if request.method == "PUT":
+        result = tickets.edit_ticket(id)
 
+    if request.method == "GET":
+        result = tickets.get_ticket(id)
+
+    if request.method == "DELETE":
+        result = tickets.delete_ticket(id)
+
+    return {"result": result}
+
+@app.route("/state", methods=['POST', 'GET'])
+def request_states():
+    result = []
+
+    if request.method == "POST":
+        result = states.create_state()
+
+    if request.method == "GET":
+        result = states.get_states()
+
+    return {"result": result}
+
+
+@app.route("/state/<id>", methods=['PUT', 'GET', 'DELETE'])
+def request_state(id):
+    result = []
+
+    if request.method == "PUT":
+        result = states.edit_state(id)
+
+    if request.method == "GET":
+        result = states.get_state(id)
+
+    if request.method == "DELETE":
+        result = states.delete_state(id)
+
+    return {"result": result}
 
 @app.route("/message", methods=['POST'])
-def request_message():
-    error = False
-    if request.method == 'POST':
+def request_messages():
+    result = []
 
-        if request.is_json:
-            data = request.get_json()
-            new_message = Message(user=data['user'],
-                ticket=data['ticket'], message=data['message'])
-            db.session.add(new_message)
-            db.session.commit()
-            result = [
-                {
-                    "message": f"Message {new_message.id} has been created successfully.",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
-        else:
-            error = True
-            result = [
-                {
-                    "message": "The request payload is not in JSON format",
-                    "data": [],
-                    "count": 0,
-                    "error": error
-                }
-            ]
-            return {"result": result}
+    if request.method == "POST":
+        result = messages.create_message()
+
+    return {"result": result}
+
+
+@app.route("/message/<id>", methods=['GET'])
+def request_message(id):
+    result = []
+
+    if request.method == "GET":
+        result = messages.get_message(id)
+
+    return {"result": result}
 
 
 if __name__ == '__main__':

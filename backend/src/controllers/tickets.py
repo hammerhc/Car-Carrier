d687450e84eaab6f53901fe9f0b1befe27194a59
hammerhc <kriegersimon4@gmail.com>
2021-06-02 09:34:02 +0200

    Changed ticket controller

--- backend/src/controllers/tickets.py
+++ backend/src/controllers/tickets.py
@@ -4,16 +4,105 @@ from app import db
 import models
 
 def create_ticket():
-    pass
+    if request.is_json:
+        data = request.get_json()
+        new_ticket = models.Ticket(
+            title=data['title'], owner=data['owner'], customer=data['customer'], state=data['state'])
+        db.session.add(new_ticket)
+        db.session.commit()
+        result = [
+            {
+                "message": f"Ticket {new_ticket.title} has been created successfully.",
+                "data": [],
+                "count": 0,
+                "error": False
+            }
+        ]
+        return result
+    else:
+        abort(400)
 
 def get_tickets():
-    pass
+    tickets = models.Ticket.query.all()
+    data = [
+        {
+            "id": ticket.id,
+            "title": ticket.title,
+            "owner": ticket.owner,
+            "customer": ticket.customer,
+            "state": ticket.state,
+            "date": ticket.date
+        } for ticket in tickets
+    ]
+
+    result = [
+        {
+            "message": "Success",
+            "data": data,
+            "count": len(data),
+            "error": False
+        }
+    ]
+    
+    return result
 
 def get_ticket(id):
-    pass
+    ticket = models.Ticket.query.get(id)
+    data = {
+        "id": ticket.id,
+        "title": ticket.title,
+        "owner": ticket.owner,
+        "customer": ticket.customer,
+        "state": ticket.state,
+        "date": ticket.date
+    }
+
+    result = [
+        {
+            "message": "Success",
+            "data": data,
+            "count": len(data),
+            "error": False
+        }
+    ]
+
+    return result
 
 def edit_ticket(id):
-    pass
+    if request.is_json:
+        ticket = models.Ticket.query.get(id)
+        data = request.get_json()
+        ticket.title = data['title']
+        ticket.owner = data['owner']
+        ticket.price = data['price']   
+        ticket.customer = data['customer']
+        ticket.state = data['state']
+        db.session.commit()
+
+        result = [
+            {
+                "message": f"Ticket {ticket.title} has been edited successfully.",
+                "data": [],
+                "count": 0,
+                "error": False
+            }
+        ]
+        return result
+    else:
+        abort(400)
 
 def delete_ticket(id):
-    pass
+    ticket = models.Ticket.query.get(id)
+    ticket_title = ticket.title
+    db.session.delete(ticket)
+    db.session.commit()
+
+    result = [
+        {
+            "message": f"Ticket {ticket_title} has been deleted successfully.",
+            "data": [],
+            "count": 0,
+            "error": False
+        }
+    ]
+    return result

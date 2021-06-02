1e0aa613296dfe777461ba5815979dff5fc9cc4f
hammerhc <kriegersimon4@gmail.com>
2021-06-02 09:13:53 +0200

    Added states controller
    Used for CRUD operations

---
+++ backend/src/controllers/states.py
@@ -0,0 +1,19 @@
+from flask import request, abort
+from app import db
+
+import models
+
+def create_state():
+    pass
+
+def get_states():
+    pass
+
+def get_state(id):
+    pass
+
+def edit_state(id):
+    pass
+
+def delete_state(id):
+    pass

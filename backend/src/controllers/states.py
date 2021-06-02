ed5768f8d2235dce306892571ea00fbacf746170
hammerhc <kriegersimon4@gmail.com>
2021-06-02 09:27:01 +0200

    Changed states controller

--- backend/src/controllers/states.py
+++ backend/src/controllers/states.py
@@ -4,16 +4,93 @@ from app import db
 import models
 
 def create_state():
-    pass
+    if request.is_json:
+        data = request.get_json()
+        new_state = models.State(
+            name=data['name'])
+        db.session.add(new_state)
+        db.session.commit()
+        result = [
+            {
+                "message": f"State {new_state.name} has been created successfully.",
+                "data": [],
+                "count": 0,
+                "error": False
+            }
+        ]
+        return result
+    else:
+        abort(400)
 
 def get_states():
-    pass
+    states = models.State.query.all()
+    data = [
+        {
+            "id": state.id,
+            "name": state.name
+        } for state in states
+    ]
+
+    result = [
+        {
+            "message": "Success",
+            "data": data,
+            "count": len(data),
+            "error": False
+        }
+    ]
+    
+    return result
 
 def get_state(id):
-    pass
+    state = models.State.query.get(id)
+    data = {
+        "id": state.id,
+        "name": state.name
+    }
+
+    result = [
+        {
+            "message": "Success",
+            "data": data,
+            "count": len(data),
+            "error": False
+        }
+    ]
+
+    return result
 
 def edit_state(id):
-    pass
+    if request.is_json:
+        state = models.State.query.get(id)
+        data = request.get_json()
+        state.name = data['name']
+        db.session.commit()
+
+        result = [
+            {
+                "message": f"State {state.name} has been edited successfully.",
+                "data": [],
+                "count": 0,
+                "error": False
+            }
+        ]
+        return result
+    else:
+        abort(400)
 
 def delete_state(id):
-    pass
+    state = models.State.query.get(id)
+    state_name = state.name
+    db.session.delete(state)
+    db.session.commit()
+
+    result = [
+        {
+            "message": f"State {state_name} has been deleted successfully.",
+            "data": [],
+            "count": 0,
+            "error": False
+        }
+    ]
+    return result

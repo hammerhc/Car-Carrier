3d2af214e20b98ab7914c5f61f6f6e71ac7afe0b
hammerhc <kriegersimon4@gmail.com>
2021-06-02 09:14:22 +0200

    Added tickets controller
    Used for CRUD operations

---
+++ backend/src/controllers/tickets.py
@@ -0,0 +1,19 @@
+from flask import request, abort
+from app import db
+
+import models
+
+def create_ticket():
+    pass
+
+def get_tickets():
+    pass
+
+def get_ticket(id):
+    pass
+
+def edit_ticket(id):
+    pass
+
+def delete_ticket(id):
+    pass

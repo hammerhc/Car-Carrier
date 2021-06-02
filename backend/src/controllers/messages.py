b8758149852bcb7929cfef852c68f060da19f8d8
hammerhc <kriegersimon4@gmail.com>
2021-06-02 09:22:48 +0200

    Changed messages controller

--- backend/src/controllers/messages.py
+++ backend/src/controllers/messages.py
@@ -4,7 +4,41 @@ from app import db
 import models
 
 def create_message():
-    pass
+    if request.is_json:
+        data = request.get_json()
+        new_message = models.Message(
+            user=data['user'], ticket=data['ticket'], message=data['message'])
+        db.session.add(new_message)
+        db.session.commit()
+        result = [
+            {
+                "message": f"Message {new_message.id} has been added to ticket {new_message.ticket} successfully.",
+                "data": [],
+                "count": 0,
+                "error": False
+            }
+        ]
+        return result
+    else:
+        abort(400)
 
 def get_message(id):
-    pass
+    message = models.Message.query.get(id)
+    data = {
+        "id": message.id,
+        "user": message.user,
+        "ticket": message.ticket,
+        "message": message.message,
+        "datetime": message.datetime
+    }
+
+    result = [
+        {
+            "message": "Success",
+            "data": data,
+            "count": len(data),
+            "error": False
+        }
+    ]
+
+    return result
